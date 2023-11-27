d7c08cc4462d5a5e64f628ac5032459f16f9d3d8
Ecxho <101029252+Ecxho@users.noreply.github.com>
2023-11-27 18:21:18 -0500

    Update test.c

--- test.c
+++ test.c
@@ -61,7 +61,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("Rev_SRSA2");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("Rev_Mag_SRSA2_10Rnd");
-		addMags(player, "Rev_Mag_SRSA2_10Rnd", 4);
+		addMags(player, "Rev_Mag_SRSA2_10Rnd", 8);
 
 		return gun;
 	}
@@ -71,7 +71,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("Rev_M2010_Blue");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("Rev_Mag_M2010_10Rnd");
-		addMags(player, "Rev_Mag_M2010_10Rnd", 4);
+		addMags(player, "Rev_Mag_M2010_10Rnd", 8);
 
 		return gun;
 	}
@@ -81,7 +81,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("PITSTOP_M10_Tan");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("PITSTOP_M10_Magazine");
-		addMags(player, "PITSTOP_M10_Magazine", 4);
+		addMags(player, "PITSTOP_M10_Magazine", 8);
 
 		return gun;
 	}
@@ -92,7 +92,7 @@ class CustomMission: MissionServer
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("Rev_M200_Suppressor");
 		gun.GetInventory().CreateAttachment("Rev_Mag_M200_7Rnd");
-		addMags(player, "Rev_Mag_M200_7Rnd", 4);
+		addMags(player, "Rev_Mag_M200_7Rnd", 8);
 
 		return gun;
 	}
@@ -102,7 +102,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("BO_M300");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("BO_Mag_M300_7rnd");
-		addMags(player, "BO_Mag_M300_7rnd", 4);
+		addMags(player, "BO_Mag_M300_7rnd", 8);
 
 		return gun;
 	}
@@ -112,7 +112,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("PITSTOP_MSR");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("PITSTOP_MSR_Magazine");
-		addMags(player, "PITSTOP_MSR_Magazine", 4);
+		addMags(player, "PITSTOP_MSR_Magazine", 8);
 
 		return gun;
 	}
@@ -122,7 +122,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("Rev_M96");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("Rev_Mag_M96_5rnd");
-		addMags(player, "Rev_Mag_M96_5rnd", 4);
+		addMags(player, "Rev_Mag_M96_5rnd", 8);
 
 		return gun;
 	}
@@ -132,7 +132,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("Rev_CDX50");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("Rev_Mag_CDX50_5Rnd");
-		addMags(player, "Rev_Mag_CDX50_5Rnd", 4);
+		addMags(player, "Rev_Mag_CDX50_5Rnd", 8);
 
 		return gun;
 	}
@@ -143,7 +143,7 @@ class CustomMission: MissionServer
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("SNAFUAX50_Mag");
 		gun.GetInventory().CreateAttachment("SNAFU_50bmg_Suppressor_BK");
-		addMags(player, "SNAFUAX50_Mag", 4);
+		addMags(player, "SNAFUAX50_Mag", 8);
 
 		return gun;
 	}
@@ -153,7 +153,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("BO_M82A1");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("BO_Mag_M82A1_10rnd");
-		addMags(player, "BO_Mag_M82A1_10rnd", 4);
+		addMags(player, "BO_Mag_M82A1_10rnd", 8);
 
 		return gun;
 	}
@@ -163,7 +163,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("A2M107");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("A2M107_Magazine");
-		addMags(player, "A2M107_Magazine", 4);
+		addMags(player, "A2M107_Magazine", 8);
 
 		return gun;
 	}
@@ -173,7 +173,7 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("A2AS50");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("Rev_Mag_SRSA2_10Rnd");
-		addMags(player, "A2AS50_Magazine", 4);
+		addMags(player, "A2AS50_Magazine", 8);
 
 		return gun;
 	}
@@ -183,7 +183,44 @@ class CustomMission: MissionServer
 		EntityAI gun = player.GetHumanInventory().CreateInHands("LF_ASR12_White");
 		gun.GetInventory().CreateAttachment("HuntingOptic");
 		gun.GetInventory().CreateAttachment("LF_ASR12_Mag");
-		addMags(player, "LF_ASR12_Mag", 4);
+		addMags(player, "LF_ASR12_Mag", 8);
+
+		return gun;
+	}
+
+	EntityAI sr100Class(PlayerBase player)
+	{
+		EntityAI gun = player.GetHumanInventory().CreateInHands("PITSTOP2_SR100_Black");
+		gun.GetInventory().CreateAttachment("HuntingOptic");
+		gun.GetInventory().CreateAttachment("PITSTOP2_SR100__Magazine");
+		addMags(player, "PITSTOP2_SR100__Magazine", 6);
+
+		return gun;
+	}
+
+	EntityAI ar50Class(PlayerBase player)
+	{
+		EntityAI gun = player.GetHumanInventory().CreateInHands("LOW_AR50A1_Demon");
+		gun.GetInventory().CreateAttachment("HuntingOptic");
+		addMags(player, "LOW_Ammo_20MM", 60);
+
+		return gun;
+	}
+
+	EntityAI bfgClass(PlayerBase player)
+	{
+		EntityAI gun = player.GetHumanInventory().CreateInHands("LOW_Serbu_Tan");
+		gun.GetInventory().CreateAttachment("HuntingOptic");
+		addMags(player, "LOW_Ammo_30x170", 60);
+
+		return gun;
+	}
+
+	EntityAI csrClass(PlayerBase player)
+	{
+		EntityAI gun = player.GetHumanInventory().CreateInHands("LOW_CSR50_Black");
+		gun.GetInventory().CreateAttachment("HuntingOptic");
+		addMags(player, "LOW_Ammo_30x170", 60);
 
 		return gun;
 	}
@@ -194,7 +231,7 @@ class CustomMission: MissionServer
 		gun.GetInventory().CreateAttachment("HuntingOptic");
         gun.GetInventory().CreateAttachment("M200Suppressor");
 		gun.GetInventory().CreateAttachment("Mag_ussrm200_7Rnd");
-		addMags(player, "Mag_ussrm200_7Rnd", 4);
+		addMags(player, "Mag_ussrm200_7Rnd", 6);
 
 		return gun;
 	}
@@ -202,22 +239,33 @@ class CustomMission: MissionServer
 	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
 	{
 		player.RemoveAllItems();
+
 		
-		player.GetInventory().CreateInInventory("Echo_6B43_Black");
+		Itembase 6b43 = player.GetInventory().CreateInInventory("Echo_6B43_Black");
+		6b43.SetQuantity(1);
 		player.GetInventory().CreateInInventory("MVS_Assault_Pack_Black");
 		player.GetInventory().CreateInInventory("MVS_CombatPants_Black");
 		player.GetInventory().CreateInInventory("MVS_CombatShirt_Black");
 		player.GetInventory().CreateInInventory("MilitaryBoots_Black");
 		player.GetInventory().CreateInInventory("MVS_Balaclava_Wraith");
 		player.GetInventory().CreateInInventory("TacticalGloves_Black");
-		player.GetInventory().CreateInInventory("BandageDressing");
-        
-        EntityAI helmet = player.GetInventory().CreateInInventory("MVS_Altyn_Black");
-        helmet.GetInventory().CreateAttachment("MVS_Altyn_Visor_Black");
+		Itembase bandage = player.GetInventory().CreateInInventory("BandageDressing");
+		bandage.SetQuantity(4);
+		ItemBase grizzly1 = player.GetInventory().CreateInInventory("Echo_Grizzly");
+		grizzly1.SetQuantity(4);
+		ItemBase grizzly2 = player.GetInventory().CreateInInventory("Echo_Grizzly");
+		grizzly2.SetQuantity(4);
+		ItemBase grizzly3 = player.GetInventory().CreateInInventory("Echo_Grizzly");
+		grizzly3.SetQuantity(4);
+		ItemBase grizzly4 = player.GetInventory().CreateInInventory("Echo_Grizzly");
+		grizzly4.SetQuantity(4);
+		
+        	EntityAI helmet = player.GetInventory().CreateInInventory("MVS_Altyn_Black");
+        	helmet.GetInventory().CreateAttachment("MVS_Altyn_Visor_Black");
 
 		EntityAI primary;
 
-		switch (Math.RandomInt(0, 13)) {
+		switch (Math.RandomInt(0, 17)) {
 			case 0: primary = srsClass(player); break;
 			case 1: primary = m2010Class(player); break;
 			case 2: primary = sakoClass(player); break;
@@ -231,15 +279,27 @@ class CustomMission: MissionServer
 			case 10: primary = m107Class(player); break;
 			case 11: primary = as50Class(player); break;
 			case 12: primary = asr12Class(player); break;
-			case 13: primary = a2m200Class(player); break;
+			case 13: primary = sr100Class(player); break;
+			case 14: primary = ar50Class(player); break;
+			case 15: primary = bfgClass(player); break;
+			case 16: primary = csrClass(player); break;
+			case 17: primary = a2m200Class(player); break;
             
 		}
         
-        player.LocalTakeEntityToHands(primary);
+        	player.LocalTakeEntityToHands(primary);
 		player.SetQuickBarEntityShortcut(primary, 0, true);
+		player.SetQuickBarEntityShortcut(bandage, 1, true);
+		player.SetQuickBarEntityShortcut(grizzly1, 2, true);
+		player.SetQuickBarEntityShortcut(grizzly2, 3, true);
+		player.SetQuickBarEntityShortcut(grizzly3, 4, true);
+		player.SetQuickBarEntityShortcut(grizzly4, 5, true);
+		player.SetQuickBarEntityShortcut(helmet, 7, true);
+		player.SetQuickBarEntityShortcut(6b43, 8, true);
+		
         
 		player.GetStatWater().Add(4400);
-        player.GetStatEnergy().Add(19400);
+        	player.GetStatEnergy().Add(19400);
 	}
 };
 
